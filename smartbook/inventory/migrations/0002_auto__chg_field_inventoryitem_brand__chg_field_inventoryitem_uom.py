8cfb1c9c99ce53967d0db549716ce2853953e35d
geethu <geethu@technomicssolutions.com>
2014-05-26 18:40:27 +0530

    Added migration

---
+++ smartbook/inventory/migrations/0002_auto__chg_field_inventoryitem_brand__chg_field_inventoryitem_uom.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'InventoryItem.brand'
+        db.alter_column(u'inventory_inventoryitem', 'brand_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['inventory.Brand'], null=True))
+
+        # Changing field 'InventoryItem.uom'
+        db.alter_column(u'inventory_inventoryitem', 'uom_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['inventory.UnitOfMeasure'], null=True))
+    def backwards(self, orm):
+
+        # User chose to not deal with backwards NULL issues for 'InventoryItem.brand'
+        raise RuntimeError("Cannot reverse this migration. 'InventoryItem.brand' and its values cannot be restored.")
+
+        # User chose to not deal with backwards NULL issues for 'InventoryItem.uom'
+        raise RuntimeError("Cannot reverse this migration. 'InventoryItem.uom' and its values cannot be restored.")
+    models = {
+        u'inventory.brand': {
+            'Meta': {'object_name': 'Brand'},
+            'brand': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '51'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'inventory.inventoryitem': {
+            'Meta': {'object_name': 'InventoryItem'},
+            'barcode': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'brand': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.Brand']", 'null': 'True', 'blank': 'True'}),
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '10'}),
+            'description': ('django.db.models.fields.TextField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'discount_permit_amount': ('django.db.models.fields.DecimalField', [], {'default': '0', 'null': 'True', 'max_digits': '14', 'decimal_places': '3', 'blank': 'True'}),
+            'discount_permit_percentage': ('django.db.models.fields.DecimalField', [], {'default': '0', 'null': 'True', 'max_digits': '14', 'decimal_places': '3', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'selling_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'}),
+            'tax': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'}),
+            'unit_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'}),
+            'uom': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.UnitOfMeasure']", 'null': 'True', 'blank': 'True'})
+        },
+        u'inventory.openingstock': {
+            'Meta': {'object_name': 'OpeningStock'},
+            'discount_permit_amount': ('django.db.models.fields.DecimalField', [], {'default': '0', 'null': 'True', 'max_digits': '14', 'decimal_places': '3', 'blank': 'True'}),
+            'discount_permit_percentage': ('django.db.models.fields.DecimalField', [], {'default': '0', 'null': 'True', 'max_digits': '14', 'decimal_places': '3', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.InventoryItem']"}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'selling_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'}),
+            'unit_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'})
+        },
+        u'inventory.unitofmeasure': {
+            'Meta': {'object_name': 'UnitOfMeasure'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'uom': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['inventory']
