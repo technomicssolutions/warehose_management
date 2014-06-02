2d28198176f6fea3931d683dbe6f3b9e13c514a9
geethu <geethu@technomicssolutions.com>
2014-06-02 10:57:04 +0530

    Changes to the url

--- smartbook/smartbook/urls.py
+++ smartbook/smartbook/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
+	# url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('web.urls')),
 	url(r'^purchase/', include('purchase.urls')),
 	url(r'^sales/', include('sales.urls')),
